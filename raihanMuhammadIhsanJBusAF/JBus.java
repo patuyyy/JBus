562bd81ec10f52d3e172234642653a3a86657bdc
Raihan M Ihsan <han.ihsan01@gmail.com>
2023-09-08 11:24:02 +0700

    CS01 - Menyelesaikan CS Modul 1

---
+++ raihanMuhammadIhsanJBusAF/JBus.java
@@ -0,0 +1,83 @@
+package raihanMuhammadIhsanJBusAF;
+
+
+/**
+ *CS 1
+ *Raihan Muhammad Ihsan - 2206028232
+ */
+public class JBus
+{
+    
+    public static void main(String args[])
+    {
+
+    }
+
+    /**
+     *KERJA CERDAS
+     */
+    public static int getBusId()
+    {
+        return 0;
+    }
+    
+    public static String getBusName()
+    {
+        return "Bus";
+    }
+    
+    public static boolean isDiscount()
+    {
+        return true;
+    }
+    
+    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount)
+    {
+   
+        if (beforeDiscount > afterDiscount)
+        {
+            return 0.0f;
+        }
+        else
+        {
+            return ((beforeDiscount - afterDiscount)/beforeDiscount)*100;
+        }
+    }
+    
+    public static int getDiscountPrice(int price, float discountPercentage)
+    {   if(discountPercentage > 100.0f)
+        {
+            return 0;
+        }
+        else
+        {
+            float temp = price - ((price * discountPercentage)/100);
+            return (int) temp;
+        }
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage)
+    {
+        float temp = discountedPrice * (100.0f/(100.0f-discountPercentage));
+        return (int) temp;
+    }
+    
+    public static float getAdminFeePercentage()
+    {
+        return 0.05f;
+    }
+    
+    public static int getAdminFee(int price)
+    {
+        float fee = getAdminFeePercentage();
+        float temp = price*fee;
+        return (int) temp;
+    }
+    
+    public static int getTotalPrice(int price, int numberOfSeat)
+    {
+        float fee = getAdminFeePercentage();
+        float temp = (price*numberOfSeat) + (price*numberOfSeat*fee);
+        return (int) temp;
+    }
+}
